79823bc6cd8ab9f41bdd0e2a308a4975885a86e6
pyr3n4 <pyrena@protonmail.com>
2019-09-25 12:18:35 +0200

    Corrección del return para url imágenes

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -13,9 +13,9 @@ export class NoimagePipe implements PipeTransform {
     }
   
     if ( images.length > 0){
-      //console.log(images[0].url);
+      console.log(images[0].url);
 
-      return 'images[0].url';
+      return images[0].url;
 
     } else {
 
